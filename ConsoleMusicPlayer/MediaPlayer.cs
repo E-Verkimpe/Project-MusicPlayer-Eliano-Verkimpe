4da6e9ae91adb742ba789772658ec01c979f1376
elian <elian@DESKTOP-MK52IV4>
2022-05-03 13:52:37 +0200

    Add project files.

---
+++ ConsoleMusicPlayer/MediaPlayer.cs
@@ -0,0 +1,65 @@
+﻿using WMPLib;
+
+namespace ConsoleMusicPlayer
+{
+    public class MediaPlayer
+    {
+        private WindowsMediaPlayer player = new WindowsMediaPlayer();
+
+        public string MusicFolder { get; private set; }
+
+        public MediaPlayer()
+        {
+            PrintTitle();
+            player.settings.volume = 5;
+        }
+
+        private void PrintTitle()
+        {
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "-- Media Player -- "));
+        }
+
+        private string GetInput(string requestedInfo)
+        {
+            Console.WriteLine(requestedInfo);
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        public void GetSongFile()
+        {
+            string filePath = "";
+
+            while (filePath == "")
+            {
+                string path = GetInput("Please enter a song file including the path.");
+
+                if (path == "-")
+                {
+                    filePath = path;
+                }
+                else if (Path.GetExtension(path) != ".mp3")
+                {
+                    Console.WriteLine("File is not an mp3");
+                }
+                else if (!File.Exists(path))
+                {
+                    Console.WriteLine("Incorrect filepath entered");
+                }
+                else
+                {
+                    filePath = path;
+                }
+            }
+
+            Console.Clear();
+            PrintTitle();
+            MusicFolder = filePath;
+        }
+
+        public void PlaySong()
+        {
+            player.URL = MusicFolder;
+        }
+    }
+}
---
+++ ConsoleMusicPlayer/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Opdracht
+In de eerste fase maak je een sterk vereenvoudigde versie van een muziekspeler. 
+Probeer hiervoor de volgende functionaliteiten te implementeren: 
+• Print een titel voor de applicatie naar de console 
+• Vraag de gebruiker om de locatie van het MP3-bestand in te voeren dat hij/zij wenst af te spelen (invoer via Console) 
+• Nadat de gebruiker de locatie heeft ingevoerd, wordt het liedje op deze locatie afgespeeld 
+• Wanneer de gebruiker op -drukt, wordt de applicatie afgesloten 
+*/
+
+using ConsoleMusicPlayer;
+
+MediaPlayer musicPlayer = new MediaPlayer();
+
+bool keepLooping = true;
+while (keepLooping)
+{
+    musicPlayer.GetSongFile();
+
+    if (musicPlayer.MusicFolder == "-")
+    {
+        keepLooping = false;
+    }
+    else
+    {
+        musicPlayer.PlaySong();
+    }
+}
+
